c2e38c3a80f6ad0a9ea5b34d0c023c50f7a5fa2d
Matheus Vicente <mvicente@outlook.com.br>
2017-10-05 20:18:07 -0300

    Função de Altarar telas Pronta

--- TheCalendarCompose/www/scripts/index.js
+++ TheCalendarCompose/www/scripts/index.js
@@ -1,1 +1,10 @@
-﻿
+﻿//Função que troca as telas
+function alterarTela() {
+    $('.targetPage').click(function () {
+        var target = $(this).attr('dt-page');
+        $('.page').removeClass('page-active');
+        $(target).addClass('page-active');
+    });
+}
+
+alterarTela();
