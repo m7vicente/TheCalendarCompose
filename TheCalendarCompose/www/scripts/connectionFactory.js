b97cbaab20d90fefdd1181bfb5df6ea226120305
Matheus Vicente <mvicente@outlook.com.br>
2017-11-06 14:15:46 -0200

    nova linha no banco de dados

--- TheCalendarCompose/www/scripts/connectionFactory.js
+++ TheCalendarCompose/www/scripts/connectionFactory.js
@@ -18,6 +18,7 @@ function populateDB(tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS tb_agendamentos(id_agendamento INTEGER PRIMARY KEY AUTOINCREMENT, fk_id_servico INTEGER NOT NULL, fk_id_pessoa_consumidor INTEGER NOT NULL, horario_dia_agendamento DATETIME, local_agendamento TEXT, valor_agendamento REAL, doc_consumidor TEXT, FOREIGN KEY (fk_id_pessoa_consumidor) REFERENCES tb_pessoa(id_pessoa), FOREIGN KEY (fk_id_servico) REFERENCES tb_servicos (id_servico) )');
     tx.executeSql("INSERT INTO tb_pessoa (nomeUsuario, senha, nome_pessoa, sexo ,nascimento, email, celular, endereco_rua, endereco_cidade, endereco_cep, endereco_estado) VALUES ('mc','he',' Matheus Guilherme de Araujo Vicente ',' Maculino ',' 1999-02-24 ',' mvicente@outlook.com.br ',' (11) 5121-3599 ',' Irmão nicolau da fonseca ',' São Paulo ', 03590-170,' SP ')");
     tx.executeSql("INSERT INTO tb_servicos (fk_id_pessoa_prestador, nome_servico, descricao_servico) VALUES (1,'HE GAY',' Matheus Guilherme de Araujo Vicente ')");
+    tx.executeSql("INSERT INTO tb_servicos (fk_id_pessoa_prestador, nome_servico, descricao_servico) VALUES (1,'Barbie',' A boneca que é a alegria da garotada ')");
 
 }
 
