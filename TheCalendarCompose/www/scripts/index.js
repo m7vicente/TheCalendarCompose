3dfe52414c44bc14ab3791a5f15ef2fb6832ea62
Matheus Vicente <mvicente@outlook.com.br>
2017-11-06 16:53:31 -0200

    Icones dos cards dos meus servicos alterados

--- TheCalendarCompose/www/scripts/index.js
+++ TheCalendarCompose/www/scripts/index.js
@@ -40,14 +40,16 @@ function preencherTelaServicos(ListaServicos) {
             '<div class="servicoCard-descricao"></div>' + ListaServicos[i].descricao_servico + '</div>' +
             '<div class="mdl-card__actions mdl-card--border">' +
 
-            '<button class="mdl-button mdl-js-button mdl-js-ripple-effect" dt-info="info" dt-page="#form-agendar" name="btnAgendar">' +
-            '<i class="material-icons">check_circle</i> Agendar' +
+            '<button class="mdl-button mdl-js-button mdl-js-ripple-effect" dt-info="info" id="servico['+ i +']" name="btnAgendar">' +
+            '<i class="material-icons">build</i> Editar' +
             '</button>' +
 
-            '<button class="mdl-button mdl-js-button mdl-js-ripple-effect targetPage" dt-atual="#servicos" dt-page="#tela-detalhes" name="btnDetalhes">' +
-            '<i id="iconDetalhes" class="material-icons">details</i> Detalhes' +
+            '<button class="mdl-button mdl-js-button mdl-js-ripple-effect targetPage" dt-page="#tela-detalhes" name="btnDesativar">' +
+            '<i id="iconDetalhes" class="material-icons">block</i> Excluir' +
             '</button></div></div></div>');
     }
+
+
 }
 
 
